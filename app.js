a0c3363e1d57c98bb5893e9bee13e82a1cafbef6
noesis01 <eupharia19@gmail.com>
2023-08-19 19:21:06 +0900

    jade

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'jade');
+
+const port = 9000;
+const host = '127.0.0.1';
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello Home Page');
+});
+
+app.get('/route', (req, res) => {
+    res.send('nodejs router');
+});
+
+app.listen(port, () => {
+    console.log(`${port} connected!`);
+});
