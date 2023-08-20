2bfe679b4d74fea8293e73d4c784c1d5c5c846d1
noesis01 <eupharia19@gmail.com>
2023-08-20 12:22:25 +0900

    post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 app.locals.pretty = true;
 
 app.set('view engine', 'jade');
@@ -18,9 +19,47 @@ app.get('/route', (req, res) => {
     res.send('nodejs router');
 });
 
+app.get('/topic/:id', (req, res) => {
+    var topic = [
+        'Javasciprt',
+        'Nodejs',
+        'Express'
+    ];
+    var output = `
+    <a href="/topic?id=0">Javascript</a><br>
+    <a href="/topic?id=1">Nodejs</a><br>
+    <a href="/topic?id=2">Express</a><br>
+    ${topic[req.params.id]}
+    `;
+
+    res.send(output);
+});
+
+app.get('/topic/:id/:mode', (req, res) => {
+    res.send(req.params.id+','+req.params.mode)
+});
+
 app.get('/template', (req, res) => {
     res.render('temp', {time:Date(), _title:'Jade Template'});
 });
+
+app.get('/form', (req, res) => {
+    res.render('form');
+});
+
+// app.get('/form_receiver', (req, res) => {
+//     var title = req.query.title;
+//     var description = req.query.description;
+//     res.send(title + ',' + description);
+// });
+
+app.post('/form_receiver', (req, res) => {
+    var title = req.body.title;
+    var des = req.body.description;
+
+    res.send(title + ',' + des);
+});
+
 
 app.listen(port, () => {
     console.log(`${port} connected!`);
